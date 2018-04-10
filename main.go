69a0644e9a2adae3ef5befa54ce90c7212f98007
Greg Herlein <gherlein@herlein.com>
2018-04-09 17:27:52 -0700

    basic mqtt working

--- main.go
+++ main.go
@@ -12,6 +12,9 @@ import (
 var (
 	xbe   *XBevent
 	debug bool = true
+
+	network string = "tcp"
+	url     string = "rpisoar:1883"
 )
 
 func init() {
@@ -20,18 +23,15 @@ func init() {
 
 func main() {
 
-	// Create an MQTT Client.
 	cli := client.New(&client.Options{
-		// Define the processing of the error handler.
 		ErrorHandler: func(err error) {
 			fmt.Println(err)
 		},
 	})
 
-	// Connect to the MQTT Server.
 	err := cli.Connect(&client.ConnectOptions{
-		Network:  "tcp",
-		Address:  "iot.eclipse.org:1883",
+		Network:  network,
+		Address:  url,
 		ClientID: []byte("example-client"),
 	})
 	if err != nil {
@@ -44,23 +44,32 @@ func main() {
 			continue
 		}
 
-		// Publish a message.
-		err = cli.Publish(&client.PublishOptions{
-			QoS:       mqtt.QoS0,
-			TopicName: []byte("bar/baz"),
-			Message:   []byte("testMessage"),
-		})
-		if err != nil {
-			panic(err)
-		}
-
 		if xbe.Code == LJOYX || xbe.Code == LJOYY || xbe.Code == RJOYX || xbe.Code == RJOYY {
 			if debug {
 				fmt.Printf("%s  x: %d   y: %d\n", xbe.Name, xbe.X, xbe.Y)
 			}
 
+			msg := fmt.Sprintf("%s|%d|%d", xbe.Name, xbe.X, xbe.Y)
+			err = cli.Publish(&client.PublishOptions{
+				QoS:       mqtt.QoS0,
+				TopicName: []byte("xb/1/joy"),
+				Message:   []byte(msg),
+			})
+			if err != nil {
+				panic(err)
+			}
+
 		} else {
 
+			msg := fmt.Sprintf("%s", xbe.Name)
+			err = cli.Publish(&client.PublishOptions{
+				QoS:       mqtt.QoS0,
+				TopicName: []byte("xb/1/raw"),
+				Message:   []byte(msg),
+			})
+			if err != nil {
+				panic(err)
+			}
 			if debug {
 				fmt.Println(xbe.Name)
 			}
