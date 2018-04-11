046f51154b205b1aa29a25ad6ec41ace421fa438
Greg Herlein <gherlein@herlein.com>
2018-04-11 07:03:20 -0700

    basically working

--- main.go
+++ main.go
@@ -9,12 +9,17 @@ import (
 )
 
 var (
-	xbe    *XBevent
-	debug  bool   = true
-	topic  string = "pi-blaster-mqtt/text"
-	broker string = "tcp://rpisoar:1883"
-	qos    int    = 0
-	m1pin  int    = 4
+	xbe         *XBevent
+	debugraw    bool   = false
+	debugjoy    bool   = true
+	debugbutton bool   = true
+	broker      string = "tcp://rpisoar:1883"
+	raw         string = "xb/1/joy-xy"
+	angles      string = "xb/1/joy-vector"
+	buttons     string = "xb/1/buttons"
+	qos         int    = 0
+	xmult       int16  = 1
+	ymult       int16  = -1
 )
 
 func init() {
@@ -37,20 +42,47 @@ func main() {
 		}
 
 		if xbe.Code == LJOYX || xbe.Code == LJOYY || xbe.Code == RJOYX || xbe.Code == RJOYY {
-			if debug {
+			if debugraw {
 				fmt.Printf("%s  x: %d   y: %d\n", xbe.Name, xbe.X, xbe.Y)
 			}
-
-			p := float64(xbe.Y) / float64(math.MaxInt16)
-			v := float64(0.05) * p
+			xbe.X *= xmult
+			xbe.Y *= ymult
+			var p float64 = 0.0
+			var msg string = "*|*|*"
+			if xbe.Code == LJOYX {
+				p = float64(xbe.X) / float64(math.MaxInt16)
+				msg = fmt.Sprintf("L|X|%f", p)
+			}
+			if xbe.Code == LJOYY {
+				p = float64(xbe.Y) / float64(math.MaxInt16)
+				msg = fmt.Sprintf("L|Y|%f", p)
+			}
+			if xbe.Code == RJOYX {
+				p = float64(xbe.X) / float64(math.MaxInt16)
+				msg = fmt.Sprintf("R|X|%f", p)
+			}
+			if xbe.Code == RJOYY {
+				p = float64(xbe.Y) / float64(math.MaxInt16)
+				msg = fmt.Sprintf("R|Y|%f", p)
+			}
+			/* this will set the PWM width - only here for testing
+			 v := float64(0.05) * p
 			v = 0.15 + v
 			msg := fmt.Sprintf("%d=%f", m1pin, v)
-			fmt.Printf("%s\n", msg)
-
-			token := client.Publish(topic, byte(qos), false, msg)
+			*/
+			if debugjoy {
+				fmt.Printf("%s\n", msg)
+			}
+			token := client.Publish(raw, byte(qos), false, msg)
 			token.Wait()
 		} else {
-
+			var msg string = "*|*|*"
+			msg = fmt.Sprintf("%s", xbe.Name)
+			if debugbutton {
+				fmt.Println(msg)
+			}
+			token := client.Publish(raw, byte(qos), false, msg)
+			token.Wait()
 			if xbe.Code == A_DOWN {
 
 			}
@@ -58,9 +90,6 @@ func main() {
 
 			}
 
-			if debug {
-				fmt.Println(xbe.Name)
-			}
 		}
 
 	}
